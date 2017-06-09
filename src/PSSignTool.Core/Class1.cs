ae9bc369e3fc742990aab068b71a799cbb8b0ef5
Oren Novotny <oren@novotny.org>
2017-06-08 20:49:33 -0400

    Initial proj structure

---
+++ src/PSSignTool.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PSSignTool.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/PSSignTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PSSignTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
